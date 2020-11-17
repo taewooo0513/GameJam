12e1355ea59f7af7db6d69bc301aadf3b35473b0
taewooo0513 <taewooo0513@gmail.com>
2020-11-17 15:00:30 +0900

    gd

--- Assets/Script/BlockSpawn.cs
+++ Assets/Script/BlockSpawn.cs
@@ -4,11 +4,27 @@ using UnityEngine;
 
 public class BlockSpawn : MonoBehaviour
 {
+    public Transform Cam;
     public Transform[] SpawnPos = new Transform[4];
     public GameObject NomalBlock;
+    public GameObject GreenBlock;
+    public GameObject PinkBlock;
+    public GameObject BlueBlock;
+    bool BlueStart = false;
+    private float size = 5;
+    private bool pinkStart;
+    private float randSize;
+    private bool GreenStart;
     private int Rand;
-    public float SetTime, CurTime;
-    public List<GameObject>ObjectList = new List<GameObject>();
+    private bool YellowStop;
+    public float BlueCurTime,BlueSpawnStart,GameTime,SetTime, CurTime, GreenSpawnTime, GreenCurTime, GreenStartSpawnTime,CamRotStart,CamRotCurTime,PinkStartSpawn,PinkCurTime,ShakeStartTime;
+    public List<GameObject> ObjectList = new List<GameObject>();
+    public List<GameObject> GreenList = new List<GameObject>();
+    public List<GameObject> PinkList = new List<GameObject>();
+    public List<GameObject> BlueList = new List<GameObject>();
+
+
+    private float Camrot;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,13 +34,100 @@ public class BlockSpawn : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        BlueCurTime += Time.deltaTime;
+        PinkCurTime += Time.deltaTime;
+        GameTime += Time.deltaTime;
+        CamRotCurTime += Time.deltaTime;
+        GreenCurTime += Time.deltaTime;
         CurTime += Time.deltaTime;
         Rand = Random.Range(0, 4);
+        if(GameTime > 33&&GameTime < 36)
+        {
+            YellowStop = true;
+        }
+        if(GameTime > 36&& GameTime < 53)
+        {
+            pinkStart = true;
+        }
+        if(Camrot < 360)
+        {
+        if(CamRotCurTime > CamRotStart)
+        {
+        Camrot += 0.15f   ;
+            Cam.transform.rotation = Quaternion.EulerRotation(0,0,Camrot* Mathf.Deg2Rad);
+        }
+
+        }
+        if (GreenCurTime > GreenStartSpawnTime&&GreenCurTime < GreenStartSpawnTime+4)
+        {
+            GreenStart = true;
+        }
+        
+        if (GreenStart)
+        {
+            if(GreenCurTime>GreenSpawnTime)
+            {
+                GreenCurTime = 0;
+                GreenList.Add(Instantiate(GreenBlock, SpawnPos[Rand].transform.position, Quaternion.Euler(0, 0, (Rand * 90))));
+            }
+        }
+        if(YellowStop != true)
         if (CurTime > SetTime)
         {
-            
-            ObjectList.Add(Instantiate(NomalBlock, SpawnPos[Rand].transform.position,Quaternion.Euler(0,0,(Rand *90))));
+
+            ObjectList.Add(Instantiate(NomalBlock, SpawnPos[Rand].transform.position, Quaternion.Euler(0, 0, (Rand * 90))));
             CurTime = 0;
+        }
+        if(pinkStart == true )
+        {
+            if(size< 7&& GameTime <47)
+            {
+            size += 0.01f;
+            }
+
+            Cam.GetComponent<Camera>().orthographicSize = size;
+            if(PinkCurTime > PinkStartSpawn)
+            {
+
+                ObjectList.Add(Instantiate(PinkBlock, SpawnPos[Rand].transform.position, Quaternion.Euler(0, 0, (Rand * 90))));
+                PinkCurTime = 0;
+            }
+        }
+        if(GameTime< 46&&GameTime > 42)
+        {
+            randSize = Random.RandomRange(65f,70f)/10f;
+            Cam.GetComponent<Camera>().orthographicSize = randSize;
+        }
+        if (GameTime >= 46 && GameTime <= 47)
+        {
+            GreenStart = false;
+            pinkStart = false;
+        }
+            if (GameTime > 47 && GameTime < 48)
+        {
+
+            if(size  > -5)
+            size -= 0.04f;
+            Cam.GetComponent<Camera>().orthographicSize = size;
+
+        }
+        if (GameTime >= 48 && GameTime <= 49)
+        {
+            BlueStart = true;
+        }
+        if(BlueStart == true)
+        {
+            if(BlueCurTime > BlueSpawnStart)
+            {
+            BlueList.Add(Instantiate(BlueBlock, SpawnPos[Rand].transform.position, Quaternion.Euler(0, 0, (Rand * 90))));
+                BlueCurTime = 0;
+            }
+        }
+        Debug.Log(randSize);
+        if(GameTime > 58)
+        {
+            Cam.GetComponent<Camera>().orthographicSize = 5;
+
         }
     }
 }
--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -10,6 +10,12 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        audioSource = gameObject.AddComponent<AudioSource>();
+
+        audioSource.Stop();
+        audioSource.clip = Clip;
+        audioSource.loop = false;
+        audioSource.time = 0;
 
     }
 
@@ -29,18 +35,20 @@ public class Player : MonoBehaviour
                         {
                             Destroy(Object.transform.GetComponent<BlockSpawn>().ObjectList[0].gameObject);
                             Object.transform.GetComponent<BlockSpawn>().ObjectList.Remove(Object.transform.GetComponent<BlockSpawn>().ObjectList[0]);
+                            audioSource.Play();
                         }
                     }
 
                 }
                 else if (Object.transform.GetComponent<BlockSpawn>().ObjectList[0].transform.position.y - this.transform.position.y < 0)
                 {
-                    if (Vector3.Distance(Object.transform.GetComponent<BlockSpawn>().ObjectList[0].transform.position, this.transform.position) <3)
+                    if (Vector3.Distance(Object.transform.GetComponent<BlockSpawn>().ObjectList[0].transform.position, this.transform.position) < 3)
                     {
                         if (Input.GetKeyDown(KeyCode.DownArrow))
                         {
                             Destroy(Object.transform.GetComponent<BlockSpawn>().ObjectList[0].gameObject);
                             Object.transform.GetComponent<BlockSpawn>().ObjectList.Remove(Object.transform.GetComponent<BlockSpawn>().ObjectList[0]);
+                            audioSource.Play();
                         }
                     }
                 }
@@ -56,6 +64,7 @@ public class Player : MonoBehaviour
                         {
                             Destroy(Object.transform.GetComponent<BlockSpawn>().ObjectList[0].gameObject);
                             Object.transform.GetComponent<BlockSpawn>().ObjectList.Remove(Object.transform.GetComponent<BlockSpawn>().ObjectList[0]);
+                            audioSource.Play();
                         }
                     }
                 }
@@ -67,6 +76,130 @@ public class Player : MonoBehaviour
                         {
                             Destroy(Object.transform.GetComponent<BlockSpawn>().ObjectList[0].gameObject);
                             Object.transform.GetComponent<BlockSpawn>().ObjectList.Remove(Object.transform.GetComponent<BlockSpawn>().ObjectList[0]);
+                            audioSource.Play();
+                        }
+                    }
+                }
+            }
+
+        }
+
+        //초록블럭
+         if (Object.transform.GetComponent<BlockSpawn>().GreenList.Count != 0)
+        {
+            if (Object.transform.GetComponent<BlockSpawn>().GreenList[0].transform.position.x - this.transform.position.x == 0)
+            {
+                if (Object.transform.GetComponent<BlockSpawn>().GreenList[0].transform.position.y - this.transform.position.y > 0)
+                {
+                    if (Vector3.Distance(Object.transform.GetComponent<BlockSpawn>().GreenList[0].transform.position, this.transform.position) < 3)
+                    {
+                        if (Input.GetKeyDown(KeyCode.UpArrow))
+                        {
+                            Destroy(Object.transform.GetComponent<BlockSpawn>().GreenList[0].gameObject);
+                            Object.transform.GetComponent<BlockSpawn>().GreenList.Remove(Object.transform.GetComponent<BlockSpawn>().GreenList[0]);
+                            audioSource.Play();
+                        }
+                    }
+
+                }
+                else if (Object.transform.GetComponent<BlockSpawn>().GreenList[0].transform.position.y - this.transform.position.y < 0)
+                {
+                    if (Vector3.Distance(Object.transform.GetComponent<BlockSpawn>().GreenList[0].transform.position, this.transform.position) < 3)
+                    {
+                        if (Input.GetKeyDown(KeyCode.DownArrow))
+                        {
+                            Destroy(Object.transform.GetComponent<BlockSpawn>().GreenList[0].gameObject);
+                            Object.transform.GetComponent<BlockSpawn>().GreenList.Remove(Object.transform.GetComponent<BlockSpawn>().GreenList[0]);
+                            audioSource.Play();
+                        }
+                    }
+                }
+            }
+            if (Object.transform.GetComponent<BlockSpawn>().GreenList[0].transform.position.y - this.transform.position.y == 0)
+            {
+
+                if (Object.transform.GetComponent<BlockSpawn>().GreenList[0].transform.position.x - this.transform.position.x > 0)
+                {
+                    if (Vector3.Distance(Object.transform.GetComponent<BlockSpawn>().GreenList[0].transform.position, this.transform.position) < 3)
+                    {
+                        if (Input.GetKeyDown(KeyCode.RightArrow))
+                        {
+                            Destroy(Object.transform.GetComponent<BlockSpawn>().GreenList[0].gameObject);
+                            Object.transform.GetComponent<BlockSpawn>().GreenList.Remove(Object.transform.GetComponent<BlockSpawn>().GreenList[0]);
+                            audioSource.Play();
+                        }
+                    }
+                }
+                else if (Object.transform.GetComponent<BlockSpawn>().GreenList[0].transform.position.x - this.transform.position.x < 0)
+                {
+                    if (Vector3.Distance(Object.transform.GetComponent<BlockSpawn>().GreenList[0].transform.position, this.transform.position) < 3)
+                    {
+                        if (Input.GetKeyDown(KeyCode.LeftArrow))
+                        {
+                            Destroy(Object.transform.GetComponent<BlockSpawn>().GreenList[0].gameObject);
+                            Object.transform.GetComponent<BlockSpawn>().GreenList.Remove(Object.transform.GetComponent<BlockSpawn>().GreenList[0]);
+                            audioSource.Play();
+                        }
+                    }
+                }
+            }
+
+        }
+        //핑크블럭
+        if (Object.transform.GetComponent<BlockSpawn>().PinkList.Count != 0)
+        {
+            if (Object.transform.GetComponent<BlockSpawn>().PinkList[0].transform.position.x - this.transform.position.x == 0)
+            {
+                if (Object.transform.GetComponent<BlockSpawn>().PinkList[0].transform.position.y - this.transform.position.y > 0)
+                {
+                    if (Vector3.Distance(Object.transform.GetComponent<BlockSpawn>().PinkList[0].transform.position, this.transform.position) < 3)
+                    {
+                        if (Input.GetKeyDown(KeyCode.UpArrow))
+                        {
+                            Destroy(Object.transform.GetComponent<BlockSpawn>().PinkList[0].gameObject);
+                            Object.transform.GetComponent<BlockSpawn>().PinkList.Remove(Object.transform.GetComponent<BlockSpawn>().PinkList[0]);
+                            audioSource.Play();
+                        }
+                    }
+
+                }
+                else if (Object.transform.GetComponent<BlockSpawn>().PinkList[0].transform.position.y - this.transform.position.y < 0)
+                {
+                    if (Vector3.Distance(Object.transform.GetComponent<BlockSpawn>().PinkList[0].transform.position, this.transform.position) < 3)
+                    {
+                        if (Input.GetKeyDown(KeyCode.DownArrow))
+                        {
+                            Destroy(Object.transform.GetComponent<BlockSpawn>().PinkList[0].gameObject);
+                            Object.transform.GetComponent<BlockSpawn>().PinkList.Remove(Object.transform.GetComponent<BlockSpawn>().PinkList[0]);
+                            audioSource.Play();
+                        }
+                    }
+                }
+            }
+            if (Object.transform.GetComponent<BlockSpawn>().PinkList[0].transform.position.y - this.transform.position.y == 0)
+            {
+
+                if (Object.transform.GetComponent<BlockSpawn>().PinkList[0].transform.position.x - this.transform.position.x > 0)
+                {
+                    if (Vector3.Distance(Object.transform.GetComponent<BlockSpawn>().PinkList[0].transform.position, this.transform.position) < 3)
+                    {
+                        if (Input.GetKeyDown(KeyCode.RightArrow))
+                        {
+                            Destroy(Object.transform.GetComponent<BlockSpawn>().PinkList[0].gameObject);
+                            Object.transform.GetComponent<BlockSpawn>().PinkList.Remove(Object.transform.GetComponent<BlockSpawn>().PinkList[0]);
+                            audioSource.Play();
+                        }
+                    }
+                }
+                else if (Object.transform.GetComponent<BlockSpawn>().PinkList[0].transform.position.x - this.transform.position.x < 0)
+                {
+                    if (Vector3.Distance(Object.transform.GetComponent<BlockSpawn>().PinkList[0].transform.position, this.transform.position) < 3)
+                    {
+                        if (Input.GetKeyDown(KeyCode.LeftArrow))
+                        {
+                            Destroy(Object.transform.GetComponent<BlockSpawn>().PinkList[0].gameObject);
+                            Object.transform.GetComponent<BlockSpawn>().PinkList.Remove(Object.transform.GetComponent<BlockSpawn>().PinkList[0]);
+                            audioSource.Play();
                         }
                     }
                 }
@@ -88,6 +221,32 @@ public class Player : MonoBehaviour
             audioSource.Play();
 
             Object.transform.GetComponent<BlockSpawn>().ObjectList.Remove(other.gameObject);
+            Destroy(other.gameObject);
+        }
+        if (other.CompareTag("PinkBlock"))
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+
+            audioSource.Stop();
+            audioSource.clip = Clip;
+            audioSource.loop = false;
+            audioSource.time = 0;
+            audioSource.Play();
+
+            Object.transform.GetComponent<BlockSpawn>().ObjectList.Remove(other.gameObject);
+            Destroy(other.gameObject);
+        }
+        if (other.CompareTag("GreenBlock"))
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+
+            audioSource.Stop();
+            audioSource.clip = Clip;
+            audioSource.loop = false;
+            audioSource.time = 0;
+            audioSource.Play();
+
+            Object.transform.GetComponent<BlockSpawn>().GreenList.Remove(other.gameObject);
             Destroy(other.gameObject);
         }
     }
